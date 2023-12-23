9a15af16f8f1b00a4eb52ae0f90f047d45bf501a
Batu22 <batuhantemel14@gmail.com>
2023-12-23 23:21:58 +0300

    RouterBlog, rcc

---
+++ src/RouterBlog.jsx
@@ -0,0 +1,34 @@
+//rcc
+import React, { Component } from 'react'
+
+//CLASS
+export default class RouterBlog extends Component {
+
+    //Componentteki isim
+    static displayName = "Router_Blog"; 
+
+    //Constructor
+    constructor(props){
+        super(props);
+
+        //STATE
+        this.state={};
+
+        //BIND
+        
+    } //end constructor
+    
+    //CDM
+
+    //FUNCTION
+
+  //Render
+  render() {
+
+    //Return
+    return (
+      <div>RouterBlog</div>
+    ) //end return
+  } //end render
+} //end class
+
